63431218dd57a565dbbfe3bcf02d26d394a60b8c
sorantak <takyhsoul@gmail.com>
2021-07-25 11:58:15 +0900

    Add: persistence properties

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,9 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.CascadeType.*;
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter
@@ -19,15 +22,15 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
 //    FK가 있는 곳이 양방향 연관관계의 주인
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order ")
+    @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = ALL)
 //    연관관계 주인
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
