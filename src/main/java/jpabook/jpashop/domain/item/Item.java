7accb21d5f8ee0e1b51e6aa91f910a9fb15f71e0
sorantak <takyhsoul@gmail.com>
2021-07-28 23:52:47 +0900

    Add: Item domain

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,7 @@ import java.util.List;
 //상속관계 SINGLE_TABLE 전략
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter //@Setter
 public abstract class Item {
 
     @Id
@@ -25,5 +26,24 @@ public abstract class Item {
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<Category>();
+    private List<Category> categories = new ArrayList<>();
+
+//    비즈니스 로직(엔티티 안에 선언하는 것이 응집도가 높음/@Setter 사용하지 않고 비즈니스 로직으로 stockQuantity 증가)
+    /**
+     * 재고 증가
+     * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 감소
+     * */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity = quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("Need more stock.");
+        }
+        this.stockQuantity = restStock;
+    }
 }
